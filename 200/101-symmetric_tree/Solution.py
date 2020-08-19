1722f1a869396c5fbc2ccf0762e5dbaf47d74126
joker <740436391@qq.com>
2020-08-19 23:54:53 +0800

    101-symmetric_tree

---
+++ 200/101-symmetric_tree/Solution.py
@@ -0,0 +1,29 @@
+from TreeNodeUtil import TreeNode, make_tree_node
+
+
+class Solution:
+
+    def isSymmetric(self, root: TreeNode) -> bool:
+
+        left_ = self.left_first(root)
+        print(left_)
+        right_ = self.right_first(root)
+        print(right_)
+
+        return self.left_first(root) == self.right_first(root)
+
+    def left_first(self, node):
+
+        if node is not None:
+            return node.val, self.left_first(node.left), self.left_first(node.right)
+
+    def right_first(self, node):
+        if node is not None:
+            return node.val, self.right_first(node.right), self.right_first(node.left)
+
+
+print(
+    Solution().isSymmetric(make_tree_node((1, (2, 3, 4), (2, 4, 3)))),
+    Solution().isSymmetric(make_tree_node((1, (2, 2, None), (2, 2, None)))),
+
+)
