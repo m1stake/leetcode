22984d675ded3634b5537aa9e2a717dec4b7a4a7
joker <740436391@qq.com>
2020-08-19 23:55:28 +0800

    add tree build func

--- TreeNodeUtil.py
+++ TreeNodeUtil.py
@@ -1,8 +1,27 @@
 
 
 class TreeNode:
-    def __init__(self, x):
+
+    def __init__(self, x, left=None, right=None):
         self.val = x
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
+
+
+def make_tree_node(tree_desc):
+    """
+    :param tree_desc like (1, None, (2, 3, None))
+    """
+
+    if tree_desc is not None:
+
+        if type(tree_desc) is tuple:
+            node_val, left, right = tree_desc
+
+            return TreeNode(node_val, make_tree_node(left), make_tree_node(right))
+        else:
+            node_val = tree_desc
+            return TreeNode(node_val)
+    else:
+        return None
 
