2fbb96e7fcabe0142446bfe08e7065ec4b971355
joker <740436391@qq.com>
2020-08-22 22:53:00 +0800

    add tree travel funcs and rewrite tree.tostring

--- TreeNodeUtil.py
+++ TreeNodeUtil.py
@@ -7,6 +7,9 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return str(left_first_tree_travel(self))
+
 
 def make_tree_node(tree_desc):
     """
@@ -24,4 +27,21 @@ def make_tree_node(tree_desc):
             return TreeNode(node_val)
     else:
         return None
+
+
+def left_first_tree_travel(self, node):
+
+    if node is not None:
+        if node.left is not None or node.right is not None:
+            return node.val, self.left_first(node.left), self.left_first(node.right)
+        else:
+            return node.val
+
+
+def right_first_travel(self, node):
+    if node is not None:
+        if node.left is not None or node.right is not None:
+            return node.val, self.right_first(node.right), self.right_first(node.left)
+        else:
+            return node.val
 
